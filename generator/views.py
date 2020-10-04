d162f048ee4055c81a33fca3eb4b59ab37dfd643
Raunit <raunitd.it.17@nsit.net.in>
2020-10-04 14:41:38 +0530

    Completed the password generator

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+import string
+import random
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html', {'list': range(6, 16)})
+
+
+def password(request):
+    result = ''
+    vals = [string.ascii_letters]
+    length = int(request.GET.get("length", "12"))
+    if request.GET.get("number"):
+        vals.append(string.digits)
+    if request.GET.get("uppercase"):
+        vals.append(string.ascii_uppercase)
+    if request.GET.get("special"):
+        vals.append(string.punctuation)
+    for i in range(length):
+        idx = random.randint(0, len(vals) - 1)
+        result += random.choice(vals[idx])
+    return render(request, 'generator/password.html', {'password': result})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('generatedPassword/', views.password, name='password'),
+    path('about', views.about, name='about')
+
 ]
